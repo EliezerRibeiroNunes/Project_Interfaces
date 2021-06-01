37accf1b65e745ce98d1b35ecf73857ce325b3e0
RNeli <69021909+EliezerRibeiroNunes@users.noreply.github.com>
2021-05-31 22:06:29 -0300

    Add files via upload

---
+++ TESTE_INTERFACE/src/EliezerRibeiroNunes/com/github/programa/FiguraGeometrica.java
@@ -0,0 +1,8 @@
+package EliezerRibeiroNunes.com.github.programa;
+
+public interface FiguraGeometrica {
+
+	Float calcularPerimetro();
+	
+	Float calcularArea();
+}
---
+++ TESTE_INTERFACE/src/EliezerRibeiroNunes/com/github/programa/Triangulo.java
@@ -0,0 +1,5 @@
+package EliezerRibeiroNunes.com.github.programa;
+
+public class Triangulo {
+
+}
